0b14dcdbd41e01d7e576665e395af94edb0d73d4
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-20 14:30:58 +0430

    Adds transactions and selects based on the agregate functions

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer,
+    id integer primary key generated always as identity,
     name varchar(100),
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
-    weight_kg decimal
+    weight_kg decimal,
+    species varchar(40)
 );
 
