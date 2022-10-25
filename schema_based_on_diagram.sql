1d6280076b0206a7b66e437d769c71fee357f287
Aseni <alusinejalloh800@gmail.com>
2022-10-25 13:48:53 +0300

    Creat schema and relationshipps in the schema_based_on_diagram file

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,39 @@ CREATE TABLE patients (
     date_of_birth DATE
 );
 
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(50),
+    admitted_at TIMESTAMP,
+);
+
+CREATE TABLE invoices (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL(10, 2),
+    generate_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    type VARCHAR(50)
+);
+
+CREATE TABLE medical_history_treatments (
+    medical_history_id INT REFERENCES medical_histories(id),
+    treatment_id INT REFERENCES treatments(id),
+    PRIMARY KEY (medical_history_id, treatment_id)
+);
+
+CREATE TABLE invoice_items (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL(10, 2),
+    quantity INT,
+    total_price DECIMAL(10, 2),
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id)
+);
+
